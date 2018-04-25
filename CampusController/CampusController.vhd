57eb44747c19fb8038a2ec3c7f85cc8ae08d6e6a
Unknown <theostangebye@gmail.com>
2018-04-24 20:59:34 -0400

    Working Read Write with GPIO pins

--- CampusController/CampusController.vhd
+++ CampusController/CampusController.vhd
@@ -3,8 +3,7 @@ Library ieee;
 use ieee.std_logic_1164.all;
 
 entity CampusController is
-port(	--gpio_0 : inout std_logic_vector(1 downto 0);
-		gpio : out std_logic_vector(3 downto 2);
+port(	gpio : inout std_logic_vector(3 downto 2);
 		ledr : out std_logic_vector(1 downto 0);
 		sw : in std_logic_vector(1 downto 0)
 	);
@@ -16,6 +15,8 @@ begin
 
 	gpio(3) <= sw(0);
 	ledr(0) <= sw(0);
+	ledr(1) <= gpio(2);
+	
 
 end a;
 		
