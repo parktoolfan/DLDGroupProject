8538d28db4a9374417e4ff904d0baf44356bd79a
Unknown <theostangebye@gmail.com>
2018-04-24 22:11:19 -0400

    4b wide 4 to 1 mux

--- CampusController/CampusController.vhd
+++ CampusController/CampusController.vhd
@@ -100,3 +100,24 @@ begin
 	In1 when S='1'else
 	"0000";
 end Behavioral;
+
+-- 4 bit wide 4 to 1 MUX 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+entity mux4_4bit is
+Port (	In0, In1, In2, In3 : in std_logic_vector(3 downto 0);
+			S0, S1 : in std_logic;
+			Z : out std_logic_vector(3 downto 0)
+		);
+end mux4_4bit;
+architecture Behavioral of mux4_4bit is
+begin
+	Z <= In0 when S0='0' and S1='0' else
+	In1 when S0='1' and S1='0' else 
+	In2 when S0='0' and S1='1' else
+	In3 when S0='1' and S1='1' else
+	"0000";
+end Behavioral;
+
