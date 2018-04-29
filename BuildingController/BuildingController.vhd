750586afaa1d84b092f5fbb735a6fbd200d312a1
Theo@Plato <theostangebye@gmail.com>
2018-04-28 23:24:48 -0400

    Commented Out Armand's code

--- BuildingController/BuildingController.vhd
+++ BuildingController/BuildingController.vhd
@@ -1,316 +1,316 @@
 Library ieee;
 use ieee.std_logic_1164.all;
 
-entity BuildingController is
-port(	gpio : inout std_logic_vector(39 downto 0);
-		ledr : out std_logic_vector(17 downto 0);
-		sw : in std_logic_vector(17 downto 0);
-		key : in std_logic_vector (3 downto 0);
-		ledg : out std_logic_vector(8 downto 0)
-	);
-end BuildingController;
-
-architecture a of BuildingController is
-
-	component BuildingHardware is
-		port(Rx1, Rx2, clk_in : IN std_logic;
-			Building_ID : IN std_logic_vector(2 downto 0);
-			clk_out, Tx1, Tx2 : out std_logic;
-			room_data_out : out std_logic_vector(3 downto 0);
-			Room_ID : out std_logic_vector(5 downto 0);
-			RegisterContents : out std_logic_vector(7 downto 0)
-			);
-	end component BuildingHardware;
-
-	signal A_rx1, A_rx2, B_rx1, B_rx2, to_clk_in: std_logic;
-	signal to_building_id0, to_building_id1: std_logic_vector(2 downto 0);
-	signal net1roomID, net2roomID : std_logic_vector(5 downto 0);
-
-begin
-
-		building_A: BuildingHardware Port map (	clk_in => to_clk_in ,
-										clk_out => gpio(14),
-										Tx1 => gpio(12),
-										Tx2 => gpio(3),
-										Rx1 => A_rx1,
-										Rx2 => A_rx2,
-										Building_ID => to_building_id0,
-										Room_ID => net1roomID,
-										room_data_out => ledr( 3 downto 0),
-										RegisterContents => ledr(17 downto 10)
-									);
-
-		building_B: BuildingHardware Port map (	clk_in => to_clk_in,
-										clk_out => gpio(23),
-										Tx1 => gpio(21),
-										Tx2 => gpio(3),
-										Rx1 => B_rx1,
-										Rx2 => B_rx2,
-										Building_ID => to_building_id1,
-										Room_ID => gpio(20 downto 15),
-										room_data_out => ledr( 7 downto 4));
-	-- get master clock signal
-	to_clk_in <= gpio(5);
-
-	-- wire up building a
-	A_rx1 <= gpio(13);
-	A_rx2 <= gpio(4);
-	gpio(11 downto 6) <= net1roomID;
-
-	-- wire up building b
-	B_rx1 <= gpio(22);
-	B_rx2 <= gpio(4);
-
-	Ledg(8) <= to_clk_in;
-
-	-- Testign LEDs
-	ledg(5 downto 0) <= net1roomID;
-	
-	
-	end a;
-
-
+-- entity BuildingController is
+-- port(	gpio : inout std_logic_vector(39 downto 0);
+-- 		ledr : out std_logic_vector(17 downto 0);
+-- 		sw : in std_logic_vector(17 downto 0);
+-- 		key : in std_logic_vector (3 downto 0);
+-- 		ledg : out std_logic_vector(8 downto 0)
+-- 	);
+-- end BuildingController;
+--
+-- architecture a of BuildingController is
+--
+-- 	component BuildingHardware is
+-- 		port(Rx1, Rx2, clk_in : IN std_logic;
+-- 			Building_ID : IN std_logic_vector(2 downto 0);
+-- 			clk_out, Tx1, Tx2 : out std_logic;
+-- 			room_data_out : out std_logic_vector(3 downto 0);
+-- 			Room_ID : out std_logic_vector(5 downto 0);
+-- 			RegisterContents : out std_logic_vector(7 downto 0)
+-- 			);
+-- 	end component BuildingHardware;
+--
+-- 	signal A_rx1, A_rx2, B_rx1, B_rx2, to_clk_in: std_logic;
+-- 	signal to_building_id0, to_building_id1: std_logic_vector(2 downto 0);
+-- 	signal net1roomID, net2roomID : std_logic_vector(5 downto 0);
+--
+-- begin
+--
+-- 		building_A: BuildingHardware Port map (	clk_in => to_clk_in ,
+-- 										clk_out => gpio(14),
+-- 										Tx1 => gpio(12),
+-- 										Tx2 => gpio(3),
+-- 										Rx1 => A_rx1,
+-- 										Rx2 => A_rx2,
+-- 										Building_ID => to_building_id0,
+-- 										Room_ID => net1roomID,
+-- 										room_data_out => ledr( 3 downto 0),
+-- 										RegisterContents => ledr(17 downto 10)
+-- 									);
+--
+-- 		building_B: BuildingHardware Port map (	clk_in => to_clk_in,
+-- 										clk_out => gpio(23),
+-- 										Tx1 => gpio(21),
+-- 										Tx2 => gpio(3),
+-- 										Rx1 => B_rx1,
+-- 										Rx2 => B_rx2,
+-- 										Building_ID => to_building_id1,
+-- 										Room_ID => gpio(20 downto 15),
+-- 										room_data_out => ledr( 7 downto 4));
+-- 	-- get master clock signal
+-- 	to_clk_in <= gpio(5);
+--
+-- 	-- wire up building a
+-- 	A_rx1 <= gpio(13);
+-- 	A_rx2 <= gpio(4);
+-- 	gpio(11 downto 6) <= net1roomID;
+--
+-- 	-- wire up building b
+-- 	B_rx1 <= gpio(22);
+-- 	B_rx2 <= gpio(4);
+--
+-- 	Ledg(8) <= to_clk_in;
+--
+-- 	-- Testign LEDs
+-- 	ledg(5 downto 0) <= net1roomID;
+--
+--
+-- 	end a;
 
 
---Building hardware, one for each building
-library ieee;
-use ieee.std_logic_1164.all;
-Entity BuildingHardware is
-	port(Rx1, Rx2, clk_in : IN std_logic;
-			Building_ID : IN std_logic_vector(2 downto 0);
-			clk_out, Tx1, Tx2 : out std_logic;
-			room_data_out : out std_logic_vector(3 downto 0);
-			Room_ID : out std_logic_vector(5 downto 0);
-			RegisterContents : out std_logic_vector(7 downto 0)
-	);
-end BuildingHardware;
 
-architecture b of BuildingHardware is
 
-	component SIPO_A_shift is
-		port(clk, clr, A : in std_logic;
-	      PO : out std_logic_vector(15 downto 0)
-				);
-	end component SIPO_A_shift;
-
-	component counter is
-		port(clk, CLR, EN : in std_logic;
-			 RCO: out std_logic;
-	       Q: out std_logic_vector(3 downto 0)
-				);
-	end component counter;
-
-	component register_file is
-		port(	src_s0 : in std_logic; --read select bits, room ids least significant bits
-				src_s1 : in std_logic;
-				des_A0 : in std_logic; --write select bits
-				des_A1 : in std_logic;
-				writeToReg : in std_logic;	--write command
-				Clk : in std_logic;
-				data_src : in std_logic; -- wire to 0
-				data : in std_logic_vector(3 downto 0); --0 and three bits of room data (data in)
-				reg0 : out std_logic_vector(3 downto 0);
-				reg1 : out std_logic_vector(3 downto 0);
-				reg2 : out std_logic_vector(3 downto 0);
-				reg3 : out std_logic_vector(3 downto 0);
-				selectedData : out std_logic_vector(3 downto 0) --data of selected register
-				);
-	end component register_file;
-
-	component tri_state_buffer_top is
-		Port (	A	: in  STD_LOGIC;    -- single buffer input
-					EN	: in  STD_LOGIC;    -- single buffer enable
-					Y	: out STD_LOGIC    -- single buffer output
-		);
-	end component;
-
-	component ls74 is
-		port(	d, clr, pre, clk : IN std_logic;
-				q : out std_logic
-				);
-	end component ls74;
-
-	component asynch_counter is
-		port(clk, CLR, EN, Load_L : in std_logic;
-			 	load_in: in std_logic_vector(3 downto 0);
-			 	RCO: out std_logic;
-			  Q: out std_logic_vector(3 downto 0)
-				);
-	end component asynch_counter;
-
-	component Comparator is
-			port(A, B: in std_logic_vector(3 downto 0);
-				  A_EQ_B: out std_logic
-			);
-	end component Comparator;
-
-	component Mux is
-			port(S0, S1 : in std_logic;
-				 A, B, C, D: in std_logic_vector(15 downto 0);
-				  Y: out std_logic_vector(15 downto 0)
-				);
-	end component Mux;
-
-	component PISO_shift is
-		port(S_L, SI, clk, clr  : in std_logic;
-			 PI: in std_logic_vector(15 downto 0);
-			  SO: out std_logic);
-	end component PISO_shift;
-
-	Signal room0Data, room1Data : std_logic_vector(3 downto 0);
-	signal aux_1, aux_2, write_sig, load, rco_1, rco_2, state_inc, equals,
-	old_equals, rising_equals, reset_st_count, roomcount_clock, tx2buffer: std_logic;
-	signal state: std_logic_vector(1 downto 0);
-	signal room_data, our_id, build_id : std_logic_vector(2 downto 0);
-	signal room_id_num, st_count_out, to_our_id, to_build_id: std_logic_vector(3 downto 0);
-	signal SIPO_out, mux_out: std_logic_vector(15 downto 0);
-
-	begin
-
-		-- send clock data out.
-		clk_out <= clk_in;
-
-		Data_in: SIPO_A_SHIFT port Map(
-			clk => clk_in,
-			A => Rx1,
-			PO => SIPO_out,
-			clr => '0'
-		); --clr?
-		room_counter: counter port Map(
-			clk => roomcount_clock, EN => '1',
-			CLR => state_inc, RCO => rco_1,
-			Q => room_id_num
-		);
-		data_storage: register_file port Map(
-			data =>  '0' & room_data,
-			Clk => clk_in,
-			src_s0 => room_id_num(0),
-			src_s1 => '0',
-			writeToReg => write_sig,
-			data_src => '0',
-			des_A0 => room_id_num(0),
-			des_A1 => '0',
-			reg0 => room0Data,
-			reg1 => room1Data
-		);	--dunno about other in/out
-		
-		-- set register contents for testing:
-		RegisterContents <= room0Data & room1Data;
-		
-		
-		dff: ls74 port Map(
-			d => equals,
-			clk => clk_in,
-			q => old_equals,
-			clr => '0',
-			pre => '0'
-		);
-		state_counter: asynch_counter port map(
-			clk => state_inc,
-			Load_L => reset_st_count,
-			CLR => rising_equals,
-			EN => '1',
-			load_in => "1100",
-			Q => st_count_out
-		);
-		clockcycle_counter: asynch_counter port map(
-			clk => clk_in,
-			Load_L => '0',
-			CLR => rising_equals,
-			EN => equals,
-			load_in => "1100",
-			Q => st_count_out,
-			RCO => rco_2
-		);
-
-		-- the comparator wanted 4 bit signals
-		to_our_id <= '0' & our_id;
-		to_build_id <= '0' & build_id;
-
-		ID_comparator: Comparator port map(
-			A => to_our_id,
-			B => to_build_id,
-			A_EQ_B => equals
-		);
-		shift_load: Mux port map(
-			S0 => state(0),
-			S1 => state(1),
-			A => "1010101010101010",
-			B => "000" & room_id_num & "0" & room_data & "00000",
-			C => "0000000000000000",
-			D => "1010101010101010",
-			Y => mux_out
-		);
-		shift_output: PISO_shift port map (
-			S_L => load,
-			SI => '0',
-			clr =>'0',
-			clk => clk_in,
-			PI => mux_out,
-			SO => tx2buffer
-		);
-
-		-- wire txto bus to tx bus with a tristate buffer
-		busBuffer : tri_state_buffer_top Port map (
-				A => tx2buffer,
-				En => equals,
-				Y => TX2
-		);
-
-		--aux_1 <= SIPO_out(11) AND SIPO_out(12) AND SIPO_out(13) AND NOT(SIPO_out(14)) AND NOT(SIPO_out(15));
-		--aux_2 <= C
-		-- SIPO_out(1) AND SIPO_out(2) AND SIPO_out(3) AND SIPO_out(4) AND SIPO_out(5) AND SIPO_out(6) AND SIPO_out(7);
-	 	process(SIPO_out, aux_1,aux_2, state, write_sig, rco_2, rco_1, equals, old_equals,st_count_out, state_inc)
-			Begin
-				if (SIPO_out(15 downto 11) = "00111") then
-					aux_1 <= '1';
-				else 
-					aux_1 <= '0';
-			  end if;
-
-				if ( SIPO_out (7 downto 1) = "1111111") then
-						aux_2 <= '1';
-				else 
-					aux_2 <= '0';
-				end if;
-
-				if ( aux_1 = '1' AND aux_2 = '1' AND state = "01") then
-						write_sig <= '1';
-				else
-						write_sig <= '0';
-				end if;
-
-				if (write_sig = '1' OR (rco_2 = '1' AND state ="01")) then
-					roomcount_clock <= '1';
-				else 
-					roomcount_clock <= '0';
-				end if;
-
-				if equals = '1' AND old_equals = '0' then
-					rising_equals <= '1';
-				else 
-					rising_equals <= '0';
-				end if;
-
-				if (rco_2 = '1' AND state = "00") OR rco_1 = '1' OR (rco_2 = '1' AND state = "10") then
-					state_inc <= '1';
-				else 
-					state_inc <= '0';
-				end if;
-
-				if st_count_out(3) = '1' OR st_count_out(2) = '1' then
-					reset_st_count <= '1';
-				else 
-					reset_st_count <= '0';
-				end if;
-
-				if  state_inc = '1' OR rco_2 ='1' then
-					load <= '1';
-				else 
-					load <= '0';
-				end if;
-
-		end process;
+-- --Building hardware, one for each building
+-- library ieee;
+-- use ieee.std_logic_1164.all;
+-- Entity BuildingHardware is
+-- 	port(Rx1, Rx2, clk_in : IN std_logic;
+-- 			Building_ID : IN std_logic_vector(2 downto 0);
+-- 			clk_out, Tx1, Tx2 : out std_logic;
+-- 			room_data_out : out std_logic_vector(3 downto 0);
+-- 			Room_ID : out std_logic_vector(5 downto 0);
+-- 			RegisterContents : out std_logic_vector(7 downto 0)
+-- 	);
+-- end BuildingHardware;
+--
+-- architecture b of BuildingHardware is
+--
+-- 	component SIPO_A_shift is
+-- 		port(clk, clr, A : in std_logic;
+-- 	      PO : out std_logic_vector(15 downto 0)
+-- 				);
+-- 	end component SIPO_A_shift;
+--
+-- 	component counter is
+-- 		port(clk, CLR, EN : in std_logic;
+-- 			 RCO: out std_logic;
+-- 	       Q: out std_logic_vector(3 downto 0)
+-- 				);
+-- 	end component counter;
+--
+-- 	component register_file is
+-- 		port(	src_s0 : in std_logic; --read select bits, room ids least significant bits
+-- 				src_s1 : in std_logic;
+-- 				des_A0 : in std_logic; --write select bits
+-- 				des_A1 : in std_logic;
+-- 				writeToReg : in std_logic;	--write command
+-- 				Clk : in std_logic;
+-- 				data_src : in std_logic; -- wire to 0
+-- 				data : in std_logic_vector(3 downto 0); --0 and three bits of room data (data in)
+-- 				reg0 : out std_logic_vector(3 downto 0);
+-- 				reg1 : out std_logic_vector(3 downto 0);
+-- 				reg2 : out std_logic_vector(3 downto 0);
+-- 				reg3 : out std_logic_vector(3 downto 0);
+-- 				selectedData : out std_logic_vector(3 downto 0) --data of selected register
+-- 				);
+-- 	end component register_file;
+--
+-- 	component tri_state_buffer_top is
+-- 		Port (	A	: in  STD_LOGIC;    -- single buffer input
+-- 					EN	: in  STD_LOGIC;    -- single buffer enable
+-- 					Y	: out STD_LOGIC    -- single buffer output
+-- 		);
+-- 	end component;
+--
+-- 	component ls74 is
+-- 		port(	d, clr, pre, clk : IN std_logic;
+-- 				q : out std_logic
+-- 				);
+-- 	end component ls74;
+--
+-- 	component asynch_counter is
+-- 		port(clk, CLR, EN, Load_L : in std_logic;
+-- 			 	load_in: in std_logic_vector(3 downto 0);
+-- 			 	RCO: out std_logic;
+-- 			  Q: out std_logic_vector(3 downto 0)
+-- 				);
+-- 	end component asynch_counter;
+--
+-- 	component Comparator is
+-- 			port(A, B: in std_logic_vector(3 downto 0);
+-- 				  A_EQ_B: out std_logic
+-- 			);
+-- 	end component Comparator;
+--
+-- 	component Mux is
+-- 			port(S0, S1 : in std_logic;
+-- 				 A, B, C, D: in std_logic_vector(15 downto 0);
+-- 				  Y: out std_logic_vector(15 downto 0)
+-- 				);
+-- 	end component Mux;
+--
+-- 	component PISO_shift is
+-- 		port(S_L, SI, clk, clr  : in std_logic;
+-- 			 PI: in std_logic_vector(15 downto 0);
+-- 			  SO: out std_logic);
+-- 	end component PISO_shift;
+--
+-- 	Signal room0Data, room1Data : std_logic_vector(3 downto 0);
+-- 	signal aux_1, aux_2, write_sig, load, rco_1, rco_2, state_inc, equals,
+-- 	old_equals, rising_equals, reset_st_count, roomcount_clock, tx2buffer: std_logic;
+-- 	signal state: std_logic_vector(1 downto 0);
+-- 	signal room_data, our_id, build_id : std_logic_vector(2 downto 0);
+-- 	signal room_id_num, st_count_out, to_our_id, to_build_id: std_logic_vector(3 downto 0);
+-- 	signal SIPO_out, mux_out: std_logic_vector(15 downto 0);
+--
+-- 	begin
+--
+-- 		-- send clock data out.
+-- 		clk_out <= clk_in;
+--
+-- 		Data_in: SIPO_A_SHIFT port Map(
+-- 			clk => clk_in,
+-- 			A => Rx1,
+-- 			PO => SIPO_out,
+-- 			clr => '0'
+-- 		); --clr?
+-- 		room_counter: counter port Map(
+-- 			clk => roomcount_clock, EN => '1',
+-- 			CLR => state_inc, RCO => rco_1,
+-- 			Q => room_id_num
+-- 		);
+-- 		data_storage: register_file port Map(
+-- 			data =>  '0' & room_data,
+-- 			Clk => clk_in,
+-- 			src_s0 => room_id_num(0),
+-- 			src_s1 => '0',
+-- 			writeToReg => write_sig,
+-- 			data_src => '0',
+-- 			des_A0 => room_id_num(0),
+-- 			des_A1 => '0',
+-- 			reg0 => room0Data,
+-- 			reg1 => room1Data
+-- 		);	--dunno about other in/out
+--
+-- 		-- set register contents for testing:
+-- 		RegisterContents <= room0Data & room1Data;
+--
+--
+-- 		dff: ls74 port Map(
+-- 			d => equals,
+-- 			clk => clk_in,
+-- 			q => old_equals,
+-- 			clr => '0',
+-- 			pre => '0'
+-- 		);
+-- 		state_counter: asynch_counter port map(
+-- 			clk => state_inc,
+-- 			Load_L => reset_st_count,
+-- 			CLR => rising_equals,
+-- 			EN => '1',
+-- 			load_in => "1100",
+-- 			Q => st_count_out
+-- 		);
+-- 		clockcycle_counter: asynch_counter port map(
+-- 			clk => clk_in,
+-- 			Load_L => '0',
+-- 			CLR => rising_equals,
+-- 			EN => equals,
+-- 			load_in => "1100",
+-- 			Q => st_count_out,
+-- 			RCO => rco_2
+-- 		);
+--
+-- 		-- the comparator wanted 4 bit signals
+-- 		to_our_id <= '0' & our_id;
+-- 		to_build_id <= '0' & build_id;
+--
+-- 		ID_comparator: Comparator port map(
+-- 			A => to_our_id,
+-- 			B => to_build_id,
+-- 			A_EQ_B => equals
+-- 		);
+-- 		shift_load: Mux port map(
+-- 			S0 => state(0),
+-- 			S1 => state(1),
+-- 			A => "1010101010101010",
+-- 			B => "000" & room_id_num & "0" & room_data & "00000",
+-- 			C => "0000000000000000",
+-- 			D => "1010101010101010",
+-- 			Y => mux_out
+-- 		);
+-- 		shift_output: PISO_shift port map (
+-- 			S_L => load,
+-- 			SI => '0',
+-- 			clr =>'0',
+-- 			clk => clk_in,
+-- 			PI => mux_out,
+-- 			SO => tx2buffer
+-- 		);
+--
+-- 		-- wire txto bus to tx bus with a tristate buffer
+-- 		busBuffer : tri_state_buffer_top Port map (
+-- 				A => tx2buffer,
+-- 				En => equals,
+-- 				Y => TX2
+-- 		);
+--
+-- 		--aux_1 <= SIPO_out(11) AND SIPO_out(12) AND SIPO_out(13) AND NOT(SIPO_out(14)) AND NOT(SIPO_out(15));
+-- 		--aux_2 <= C
+-- 		-- SIPO_out(1) AND SIPO_out(2) AND SIPO_out(3) AND SIPO_out(4) AND SIPO_out(5) AND SIPO_out(6) AND SIPO_out(7);
+-- 	 	process(SIPO_out, aux_1,aux_2, state, write_sig, rco_2, rco_1, equals, old_equals,st_count_out, state_inc)
+-- 			Begin
+-- 				if (SIPO_out(15 downto 11) = "00111") then
+-- 					aux_1 <= '1';
+-- 				else
+-- 					aux_1 <= '0';
+-- 			  end if;
+--
+-- 				if ( SIPO_out (7 downto 1) = "1111111") then
+-- 						aux_2 <= '1';
+-- 				else
+-- 					aux_2 <= '0';
+-- 				end if;
+--
+-- 				if ( aux_1 = '1' AND aux_2 = '1' AND state = "01") then
+-- 						write_sig <= '1';
+-- 				else
+-- 						write_sig <= '0';
+-- 				end if;
+--
+-- 				if (write_sig = '1' OR (rco_2 = '1' AND state ="01")) then
+-- 					roomcount_clock <= '1';
+-- 				else
+-- 					roomcount_clock <= '0';
+-- 				end if;
+--
+-- 				if equals = '1' AND old_equals = '0' then
+-- 					rising_equals <= '1';
+-- 				else
+-- 					rising_equals <= '0';
+-- 				end if;
+--
+-- 				if (rco_2 = '1' AND state = "00") OR rco_1 = '1' OR (rco_2 = '1' AND state = "10") then
+-- 					state_inc <= '1';
+-- 				else
+-- 					state_inc <= '0';
+-- 				end if;
+--
+-- 				if st_count_out(3) = '1' OR st_count_out(2) = '1' then
+-- 					reset_st_count <= '1';
+-- 				else
+-- 					reset_st_count <= '0';
+-- 				end if;
+--
+-- 				if  state_inc = '1' OR rco_2 ='1' then
+-- 					load <= '1';
+-- 				else
+-- 					load <= '0';
+-- 				end if;
+--
+-- 		end process;
 
 		--roomcount_clock <= write_sig OR (rco_2 And state = "01");
 
